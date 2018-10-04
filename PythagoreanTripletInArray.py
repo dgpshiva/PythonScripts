328509ce1e716767e73ebdbfa93c8ffaed50fd2a
Shiva <dgpshiva@gmail.com>
2018-10-03 21:10:45 -0500

    Completed Pythogorean triplet in array problem

--- PythagoreanTripletInArray.py
+++ PythagoreanTripletInArray.py
@@ -2,19 +2,23 @@ def pythagoreanTripletExists(input):
     squaredList = [i ** 2 for i in input]
     squaredList.sort()
 
-    s = set()
-    for i in range(len(squaredList)-1, -1, -1):
-        if i == len(squaredList)-1:
-            hypotnuse = squaredList[i]
-        else:
-            temp = hypotnuse - squaredList[i]
-            if temp > 0 and temp in s:
+    for i in range(len(squaredList)-1, 1, -1):
+        hypotnuse = squaredList[i]
+
+        j = 0
+        k = i - 1
+        while j<k:
+            if squaredList[j] + squaredList[k] == hypotnuse:
                 return True
-            s.add(squaredList[i])
+            elif squaredList[j] + squaredList[k] > hypotnuse:
+                k = k - 1
+            else:
+                j = j + 1
 
     return False
 
 
 if __name__ == '__main__':
     ar = [3, 1, 4, 6, 5]
+    # ar = [10, 4, 6, 12, 5]
     print pythagoreanTripletExists(ar)
