11eb2ce9aed92219ec0dba4202db054ec9fa60d6
Shiva <dgpshiva@gmail.com>
2019-03-09 16:02:50 -0600

    Better optimized code for shortest word distance

--- HashMap_ShortestWordDistance.py
+++ HashMap_ShortestWordDistance.py
@@ -1,8 +1,7 @@
 from collections import defaultdict
-import math
 
 class Solution(object):
-    def shortestDistance(self, words, word1, word2):
+    def shortestWordDistance(self, words, word1, word2):
         """
         :type words: List[str]
         :type word1: str
@@ -14,17 +13,30 @@ class Solution(object):
         for i in range(0, len(words)):
             wordIndex[words[i]].append(i)
 
-        # print wordIndex
-
         minDist = None
-        for j in wordIndex[word1]:
-            for k in wordIndex[word2]:
-               minDist = abs(j-k) if minDist is None or abs(j-k)<minDist else minDist
+        if word1 == word2:
+            indexes = wordIndex[word1]
+            indexes.sort()
+            for i in range(0, len(indexes)-1):
+                minDist = abs(indexes[i] - indexes[i+1]) if minDist is None or abs(indexes[i] - indexes[i+1]) < minDist else minDist
 
-        return minDist
+            return minDist
+        else:
+            indexesWord1 = sorted(wordIndex[word1])
+            indexesWord2 = sorted(wordIndex[word2])
+            indexWord1 = 0
+            indexWord2 = 0
+            while indexWord1 < len(indexesWord1) and indexWord2 < len(indexesWord2):
+                minDist = abs(indexesWord1[indexWord1]-indexesWord2[indexWord2]) if minDist is None or abs(indexesWord1[indexWord1]-indexesWord2[indexWord2]) < minDist else minDist
+                if indexesWord1[indexWord1] < indexesWord2[indexWord2]:
+                    indexWord1 += 1
+                else:
+                    indexWord2 += 1
+
+            return minDist
 
 
 if __name__ == '__main__':
     s = Solution()
     # print s.shortestDistance(["practice", "makes", "perfect", "coding", "makes"], "coding", "practice")
-    print s.shortestDistance(["coding", "practice", "makes", "perfect", "coding", "makes"], "coding", "makes")
+    print s.shortestWordDistance(["coding", "practice", "makes", "perfect", "coding", "makes"], "coding", "makes")
