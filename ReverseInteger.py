73cd95501808c1aae5df8a8c301c8b92d8bd2dd4
Shivsankar Ramasubramanian <SRamasubramanian@slb.com>
2019-06-13 10:47:22 -0500

    Cleaning code for Reverse Integer

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -9,47 +9,40 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        # Better solution from Leetcode 
+        # Working solution from Leetcode
         number = x
-        rev = 0
         x = -x if x < 0 else x
-        while x != 0:
-            pop = int(x % 10)
-            if x > 0 and rev > sys.maxint or (rev == sys.maxint and pop > 7):
-                return 0
-            if x < 0 and -rev < -sys.maxint-1 or (-rev == -sys.maxint-1 and pop < -8):
-                return 0
-            rev = rev * 10 + pop
-            
-            x = x // 10
         
-        return rev if number > 0 else -rev
+        rev = 0
+        while x != 0:
+            rev = rev * 10 + (x%10)
+            x = x//10
+        
+        if number > 0 and rev > pow(2, 31) - 1:
+            return 0
+        if number < 0 and -rev < -pow(2, 31):
+            return 0
+        
+        return -x if number < 0 else x
 
 
+        # Better solution from Leetcode BUT Does not pass all Leetcode tests
         # number = x
+        # rev = 0
         # x = -x if x < 0 else x
-        # digits = []
-        # count = 0
-        # output = 0
-        # while x//10!=0:
-        #     digits.append(x%10)
-        #     x = x//10
-        #     count += 1
-        # if x > 0:
-        #     digits.append(x)
+        # while x != 0:
+        #     pop = int(x % 10)
+        #     if x > 0 and rev > sys.maxint or (rev == sys.maxint and pop > 7):
+        #         return 0
+        #     if x < 0 and -rev < -sys.maxint-1 or (-rev == -sys.maxint-1 and pop < -8):
+        #         return 0
+        #     rev = rev * 10 + pop
+            
+        #     x = x // 10
+        
+        # return rev if number > 0 else -rev
 
-        # for digit in digits:
-        #     output += digit * pow(10, count)
-        #     count -= 1
-
-        # if output > pow(2, 31) - 1:
-        #     return 0
-
-        # output = -output if number < 0 else output
-
-        # return output
-
-
+        
 if __name__ == '__main__':
     s = Solution()
     print s.reverse(123)
